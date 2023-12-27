26a850f204ca2d8ed2ac6c74eb1da97a1419ecc0
KulBoy6122 <sarthakkulkarni612@gmail.com>
2023-12-27 12:45:02 +0530

    design for certification and nice-select arrow

--- src/app/components/dashboard/candidate/resume/Certification.tsx
+++ src/app/components/dashboard/candidate/resume/Certification.tsx
@@ -14,11 +14,16 @@ const Certificates: React.FC = () => {
   );
   const [currentText, setCurrentText] = useState<string>("");
 
+  const [editingIndex, setEditingIndex] = useState<number | null>(null);
+  const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
+  const [isSaveVisible, setSaveVisible] = useState<boolean>(false);
+
   const addText = () => {
     if (currentText.trim() !== ''){
     setTexts([...texts, currentText]);
     
     setCurrentText('');
+    setSaveVisible(true);
     
 }
   };
@@ -30,71 +35,144 @@ const Certificates: React.FC = () => {
         certificate: texts,
       });
       if (isUpdated) {
+        setSaveVisible(false);
         notifySuccess("updated successfully");
       } else notifyError("something went wrong try again");
     }
   };
+  const selectText = (index: number | null) => {
+    setSelectedIndex((prevIndex) => (prevIndex === index ? null : index));
+  };
 
   const editText = (index: number) => {
-    const newText = prompt("Edit:", texts[index]);
-    if (newText !== null && newText.trim() !== "") {
-      const updatedTexts = [...texts];
-      updatedTexts[index] = newText;
-      setTexts(updatedTexts);
-    }
+    setEditingIndex(index);
+    setCurrentText(texts[index]); // Set the current text to the text at the specified index
+  };
+
+  const updateText = (index: number ) => {
+    
+    const updatedTexts = [...texts];
+    updatedTexts[index] = currentText; // Update the text at the specified index
+    setTexts(updatedTexts);
+    
+    // Reset states
+    setEditingIndex(null);
+    setCurrentText('');   
+    setSelectedIndex(null); 
+    setSaveVisible(true);
+    
   };
 
   const removeText = (index: number) => {
     const updatedTexts = [...texts];
     updatedTexts.splice(index, 1);
     setTexts(updatedTexts);
+    setEditingIndex(null);
+    setSaveVisible(true);
   };
 
   return (
     <>
-      <h4 className="dash-title-three">Certification and Qualification</h4>
-      <div className="dash-input-wrapper mb-40">
-        <div className="skills-wrapper py-3 ">
-          <input
-            type="text"
-            value={currentText}
-            onChange={(e) => setCurrentText(e.target.value)}
-            style={{ borderRadius: "10px" }}
-          />
-          <div className=" d-flex ">
-            <button className="mt-3 dash-btn-two tran3s me-3" onClick={addText}>
-              Add
-            </button>
-            <button
-              className="mt-3 dash-btn-two tran3s me-3"
-              onClick={handleSave}
-            >
-              Save
-            </button>
-          </div>
-
-          <ul className="list-unstyled ">
-            {texts.map((text, index) => (
-              <li
-                className="flex items-center justify-between mb-2"
-                key={index}
+        <h4 className="dash-title-three">Certification and Qualification</h4>
+        <div className="dash-input-wrapper mb-40">
+          <div className="skills-wrapper py-3 ">
+            <input
+              type="text"
+              value={currentText}
+              onChange={(e) => setCurrentText(e.target.value)}
+              style={{ borderRadius: "10px" }}
+            />
+            <div className=" d-flex ">
+              <button className="mt-3 btn-one tran3s me-3" onClick={addText}>
+                Add
+              </button>
+              {isSaveVisible && (
+              <button
+                className="mt-3 dash-btn-two tran3s me-3 "
+                onClick={handleSave}
               >
-                <span className="font-bold text-xl">{text}</span>
-                <div className="flex">
-                  <button className="ml-2" onClick={() => editText(index)}>
-                    edit
-                  </button>
-                  <button className="ml-2" onClick={() => removeText(index)}>
-                    X
-                  </button>
-                </div>
-              </li>
-            ))}
-          </ul>
+                Save
+              </button>
+              )}
+            </div>
+          <div className="skills-wrapper"></div>
+            <ul className="  flex-wrap align-items-center ">
+              {texts.map((text, index) => (
+                <li
+                  className=" mt-3"  style={{ padding: "0 22px" }}
+                  key={index}
+                >
+                  <div className="d-flex">
+                  {editingIndex === index ? (
+                    <>
+                      <input
+                        type="text"
+                        value={currentText}
+                        onChange={(e) => setCurrentText(e.target.value)}
+                        style={{ borderRadius: "10px" }}
+                      />
+                      <div className="d-flex">
+                        <button
+                          className="btn"
+                          onClick={() => updateText(index)}
+                        >
+                          Update
+                        </button>
+                        <button
+                          className="btn"
+                          onClick={() => setEditingIndex(null)}
+                        >
+                          Cancel
+                        </button>
+                      </div>
+                    </>
+                  ) : (
+                    <>
+                      <button
+                        className="btn-eight style-two"
+                        onClick={() => selectText(index)}
+                      >
+                        {text}
+                      </button>
+                      {selectedIndex === index && (
+                        <div className="d-flex">
+                          <button
+                            className="btn"
+                            onClick={() => editText(index)}
+                          >
+                            Edit
+                          </button>
+                          <button
+                            className="btn"
+                            onClick={() => removeText(index)}
+                          >
+                            Remove
+                          </button>
+                        </div>
+                      )}
+                    </>
+                  )}
+                  {/* <button className="btn-eight style-two"onClick={() => selectText(index)}>{text}</button>
+                  {editingIndex === index && (
+                  <>
+                    <button className="btn" onClick={() => editText(index)}>edit</button>
+                    <button className="btn" onClick={() => removeText(index)}>remove</button>
+                  </>
+                )} */}
+                    {/* <button className="" onClick={() => editText(index)}>
+                      edit
+                    </button>
+                    <button className="bi bi-x" onClick={() => removeText(index)}>
+                      remove
+                    </button> */}
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
-      </div>
-    </>
-  );
-};
+      </>
+    );
+  };
 
-export default Certificates;
+  export default Certificates;
